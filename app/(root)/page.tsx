b740b72642f2764052b2cab9b9e8f3b38f539ed2
Nasir Mohammed Sani <mohammedsani149@yahoo.com>
2025-01-15 20:14:31 +0100

    implement initial components and complete layout for banking application

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/ui/HeaderBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Ekko",
+    lastName: "Ekko",
+    email: "ekkoEp@time.pro",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1000}
+          />
+        </header>
+        Recent transactions
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 150.55 }, { currentBalance: 150.0 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
